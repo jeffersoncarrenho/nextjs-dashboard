4819139118dc6d329e012955631b1e759837d50b
jeffersoncarrenho <jefferson.carrenho@gmail.com>
2024-08-06 10:04:43 -0300

    Chapter 7 finished

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { Invoice, User } from '@/app/lib/definitions';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -18,10 +19,10 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
+      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+      {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+      {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+      {/* <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
